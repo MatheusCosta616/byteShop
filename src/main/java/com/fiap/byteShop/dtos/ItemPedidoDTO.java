d465a961c5aeb41b620164aee2cb669f7594456e
MatheusCosta616 <matheus.costa2616@gmail.com>
2025-09-16 21:14:37 -0300

    add: item pedido dto

---
+++ src/main/java/com/fiap/byteShop/dtos/ItemPedidoDTO.java
@@ -0,0 +1,60 @@
+package com.fiap.byteShop.dtos;
+
+public class ItemPedidoDTO {
+    private Long id;
+    private Long pedidoId;
+    private Long produtoId;
+    private Integer quantidade;
+    private Double precoUnitario;
+
+    public ItemPedidoDTO() {
+    }
+
+    public ItemPedidoDTO(Long id, Long pedidoId, Long produtoId, Integer quantidade, Double precoUnitario) {
+        this.id = id;
+        this.pedidoId = pedidoId;
+        this.produtoId = produtoId;
+        this.quantidade = quantidade;
+        this.precoUnitario = precoUnitario;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getPedidoId() {
+        return pedidoId;
+    }
+
+    public void setPedidoId(Long pedidoId) {
+        this.pedidoId = pedidoId;
+    }
+
+    public Long getProdutoId() {
+        return produtoId;
+    }
+
+    public void setProdutoId(Long produtoId) {
+        this.produtoId = produtoId;
+    }
+
+    public Integer getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(Integer quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public Double getPrecoUnitario() {
+        return precoUnitario;
+    }
+
+    public void setPrecoUnitario(Double precoUnitario) {
+        this.precoUnitario = precoUnitario;
+    }
+}
