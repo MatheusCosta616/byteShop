c41df39ebed675b07fe0bfa4068b011040f03aad
MatheusCosta616 <matheus.costa2616@gmail.com>
2025-09-16 21:04:11 -0300

    add: pagamento repository

---
+++ src/main/java/com/fiap/byteShop/repositories/PagamentoRepository.java
@@ -0,0 +1,10 @@
+package com.fiap.byteShop.repositories;
+
+import com.fiap.byteShop.models.Pagamento;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PagamentoRepository extends JpaRepository<Pagamento, Long> {
+
+}
