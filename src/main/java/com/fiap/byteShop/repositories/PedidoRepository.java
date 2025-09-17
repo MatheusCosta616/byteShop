4501a320ecf510ebf3b5be61701818ad6ac8ad58
MatheusCosta616 <matheus.costa2616@gmail.com>
2025-09-16 21:05:04 -0300

    add: pedido repository

---
+++ src/main/java/com/fiap/byteShop/repositories/PedidoRepository.java
@@ -0,0 +1,9 @@
+package com.fiap.byteShop.repositories;
+
+import com.fiap.byteShop.models.Pedido;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PedidoRepository extends JpaRepository<Pedido, Long> {
+}
